2489a51c24e39be4f0c89da3b558a8223199b2e6
PigsGoMoo <azianknowitall@gmail.com>
2021-09-08 14:54:32 -0400

    Fixed small typo

--- PoeHideoutCalc/src/App.js
+++ PoeHideoutCalc/src/App.js
@@ -80,7 +80,7 @@ export default class App extends React.Component {
         <div>
           <button onClick={this.reset}>Upload another</button>
         </div>
-        <div class='separator'>
+        <div className='separator'>
           <form onSubmit={this.update}>
             <label htmlFor='decorUpdate'>Update your decor list:</label>
             <input
